1ead21be8121a865d588e795b34e7871976e7cd7
Kenny Batista <kennybatista@MacBook-Pro.local>
2016-11-04 14:10:25 -0700

    made my own shuffle method

--- wordChallenges.py
+++ wordChallenges.py
@@ -4,9 +4,26 @@ import sys
 import random
 
 # this variable stores the command line arguments that the file is taking in.
-list_of_command_line_arguments = sys.argv
+arrayOfArgumentsPassedIn = sys.argv
 # The shuffled variable stores the passed in arguments and randomly shuffles it by using the random module
-shuffled = random.shuffle(list_of_command_line_arguments)
+# shuffled = random.shuffle(list_of_command_line_arguments)
 
-# Here wre print
-print(list_of_command_line_arguments)
+def shuffle(arrayOfArgumentsPassedIn):
+    copy = list(arrayOfArgumentsPassedIn)
+    shuffle_in_place(copy)
+    return copy
+
+def shuffle_in_place(arrayOfArgumentsPassedIn):
+    array_len = len(arrayOfArgumentsPassedIn)
+    assert array_len > 2, 'Array is too short to shuffle!'
+    for index in range(array_len):
+        swap = random.randrange(array_len - 1)
+        swap += swap >= index
+        arrayOfArgumentsPassedIn[index], arrayOfArgumentsPassedIn[swap] = arrayOfArgumentsPassedIn[swap], arrayOfArgumentsPassedIn[index]
+
+
+array = list(range(3))
+shuffle(arrayOfArgumentsPassedIn)
+arrayOfArgumentsPassedIn
+shuffle_in_place(arrayOfArgumentsPassedIn)
+print arrayOfArgumentsPassedIn
