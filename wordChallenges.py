da9b54b7fb6e33c24bd2f1638949f0fc80f09cf4
Kenny Batista <kennybatista7@gmail.com>
2016-11-03 21:32:15 -0700

    Update wordChallenges.py

--- wordChallenges.py
+++ wordChallenges.py
@@ -1,12 +1,12 @@
-#The sys module allows us to access the command line arguments
+#The sys module allows us to access the command line arguments.
 import sys
-# the random module allows us to us functions for randomly or generating numbers or lists
+# the random module allows us to use functions for randomly generating number or shuffling list elements.
 import random
 
-# this variable stores the command line arguments that the file is taking in.
+# This variable stores the command line arguments that the file takes in.
 list_of_command_line_arguments = sys.argv
-# The shuffled variable stores the passed in arguments and randomly shuffles it by using the random module
+# The shuffled variable stores the passed in arguments and randomly shuffles them by using the random.shuffle method
 shuffled = random.shuffle(list_of_command_line_arguments)
 
-# Here wre print
+# Here we print the shuffled list
 print(list_of_command_line_arguments)
