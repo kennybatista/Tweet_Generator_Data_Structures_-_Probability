7664d14ba1fa7e08f3ceca7983266de7bf06000d
Kenny Batista <kennybatista@MacBook-Pro.local>
2016-11-04 17:26:59 -0700

    reaarange words from a list challenge completed

--- wordChallenges.py
+++ wordChallenges.py
@@ -1,24 +1,30 @@
 #The sys module allows us to access the command line arguments.
 import sys
-# the random module allows us to use functions for randomly generating number or shuffling list elements.
+# The random module allows us to use functions for randomly generating integers or shuffling a list of elements.
 import random
 
-# this variable stores the command line arguments that the file is taking in.
-arrayOfArgumentsPassedIn = sys.argv
+# CHALLENGE!: Build a script that reaaranges words in a lists.
 
-def shuffle_in_place(arrayOfArgumentsPassedIn):
-    # len returns the length of a sequence
-    array_len = len(arrayOfArgumentsPassedIn)
-    assert array_len > 2, 'Array is too short to shuffle!'
-    # range() it tells us when it'll stop shuffling.
-    for index in range(array_len):
-        swap = random.randrange(array_len - 1)
-        swap += swap >= index
-        arrayOfArgumentsPassedIn[index], arrayOfArgumentsPassedIn[swap] = arrayOfArgumentsPassedIn[swap], arrayOfArgumentsPassedIn[index]
+# PSEUDOCODE:
+# sampleData = ['kenny','is','the','awesomest','guy']
+# reaarangedList = []
+# loop through the 'sampleData' list
+# create random integer, which we'll use as a random index for the word
+# Attach the new random index to the words and add to the 'reaarangedList'
+# if there is an element that I don'w want, remove it
+# print the new rearranged list
+
+# Here we store our list of arguments.
+sample_Data = sys.argv
+# List where we will store our newly arranged words.
+rearranged_words = []
+
+# Here we loop throught the system argument list
+for word in range(1, len(sample_Data)):
+    # Ask Alan how the edge case works here
+    random_Int = random.randint(1, len(sample_Data)-1)
+    rearranged_words.append(sample_Data[random_Int])
+    sample_Data.remove(sample_Data[random_Int])
 
 
-# array = list(range(5))
-# shuffle(arrayOfArgumentsPassedIn)
-arrayOfArgumentsPassedIn
-shuffle_in_place(arrayOfArgumentsPassedIn)
-print arrayOfArgumentsPassedIn
+print(rearranged_words)
