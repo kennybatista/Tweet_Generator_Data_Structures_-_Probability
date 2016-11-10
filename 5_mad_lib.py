70a475abbef7c7e97ed4bdc9245a6441a766b784
Kenny Batista <kennybatista@MacBook-Pro.local>
2016-11-09 21:32:36 -0800

    mad lib

---
+++ 5_mad_lib.py
@@ -0,0 +1,9 @@
+sweaty = raw_input("His palms are 'ADJECTIVE': ")
+weak = raw_input("kness 'ADJECTIVE': ")
+arms = raw_input("arms are 'ADJECTIVE': ")
+spaghetti = raw_input("Theres vomit on his seater already, moms 'NOUN': ")
+calm = raw_input("Hes nervous, but on the surface he looks 'ADJECTIVE' and ready.: ")
+
+new_line = "\n"
+lyrics = new_line + "His palms are " + sweaty + ", knees " + weak + " , arms are " + arms + " Theres vomit on his sweater already, moms " + spaghetti + " Hes nervous, but on the surface he looks calm and ready To drop bombs, but he keeps on forgettin What he wrote down, the whole crowd goes so loud He opens his mouth, but the words wont come out Hes chokin, how, everybodys jokin now The clocks run out, times up, over, blaow! Snap back to reality, oh there goes gravity"
+print lyrics
