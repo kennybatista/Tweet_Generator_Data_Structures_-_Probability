fa0b99a715b913cbe009c28dd2144fb124270b6b
Kenny Batista <kennybatista@MacBook-Pro.local>
2016-11-09 22:50:49 -0800

    relocation of corpus

--- 3_dictionary_word.py
+++ 3_dictionary_word.py
@@ -12,10 +12,10 @@ import sys, random
 def read_from_file_and_output_a_list_of_words():
         # use the open() object to open the file, use 'r' for read mode.
         # If I wanted to write to the file I would use 'w'
-        word_file = open('/Users/kennybatista/Desktop/words.txt','r')
+        word_file = open('./words.txt','r')
         # Read the file and store all the data ( separated in new lines ) in a variable
         words_stored_in_list_data_structure = word_file.read()
-        # Split each new lined word and store it in a list data structure
+        # Split each new lined word and store it in a list data structures
         list_of_splitted_words_stored_in_a_list = words_stored_in_list_data_structure.split()
         # Before I added the int(), this was a list, and if i tried looping through sys.argv[1] it would just give me the amount of elements on the list. So that's why I joited them from 1,0,0 to 100 and turned it into an integer
         valueOfArgumentPassed = int(''.join(sys.argv[1]))
@@ -35,15 +35,4 @@ def read_from_file_and_output_a_list_of_words():
 # read_from_file_and_output_a_list_of_words()
 
 
-
-# def generateAnagram(string, language="en_US"):
-#    languageDict = enchant.Dict(language)
-#    numOfPossibleCombinationsForString = math.factorial(len(string))
-#    for i in range(0, numOfPossibleCombinationsForString):
-#        wordWithShuffledCharacters = shuffleCharactersOf(string)
-#        if languageDict.check(wordWithShuffledCharacters):
-#             return wordWithShuffledCharacters
-#
-#             return "There is no anagram in %s for %s." % (language, string)
-#
-# generateAnagram("Helo", "en_US")
+read_from_file_and_output_a_list_of_words()
