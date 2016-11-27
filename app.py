a2defe0c983ec5ec014d77b46dcd5e94180bfe9c
Kenny Batista <kennybatista7@gmail.com>
2016-11-27 00:29:08 -0800

    updated config file

--- app.py
+++ app.py
@@ -78,6 +78,7 @@ frequency("and", histogram)
 # we return a random word
 # stochastic_random(histogram)
 
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
